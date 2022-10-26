c6702bc0ce651400be8435b018990f2f1241ff84
Hassan El dika <hassan.el.dika8@gmail.com>
2022-10-26 09:34:35 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Cater Me',
+      title: 'CaterMe',
       theme: ThemeData(primaryColor: Colors.white),
       home: SplashScreen(),
     );
